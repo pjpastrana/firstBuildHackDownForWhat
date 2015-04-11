82a6f6bc58a1351dabd9c197c501d2c75ba6b5b3
pjpastrana <pj.pastrana@gmail.com>
2015-04-11 09:18:22 -0400

    - Removing crap from Jake - integration between cpp program and python

--- system/main.py
+++ system/main.py
@@ -1,5 +1,9 @@
 from multiprocessing import Pool
 from mcr12 import *
+import subprocess
+import os
+
+SHELFIE_EXE = "/home/root/smart_shelf"
 
 def main():
     pool = Pool(processes=2)
@@ -27,6 +31,44 @@ def check_weight_change():
 def add_item(barcode):
     print "adding item"
     print barcode
+    ad_vals = calculate_anal_dig_values()
+    print ad_vals
+    r_vals = calculate_resistance(ad_vals)
+    print r_vals
+    f_vals = calculate_force_values(r_vals)
+    print f_vals
+    weight = calculate_weight(f_vals)
+    print weight
+    quadrant = calculate_quadrant(f_vals)
+    print quadrant
+    
+
+def calculate_anal_dig_values():
+    cmd = [SHELFIE_EXE, "a"]
+    return exec_command(cmd)
+
+def calculate_resistance(ad_vals):
+    cmd = [SHELFIE_EXE, "r", ad_vals[0], ad_vals[1], ad_vals[2], ad_vals[3] ]
+    return exec_command(cmd)
+
+def calculate_force_values(r_vals):
+    cmd = [SHELFIE_EXE, "f", r_vals[0], r_vals[1], r_vals[2], r_vals[3] ]
+    return exec_command(cmd)
+
+def calculate_weight(f_vals):
+    cmd = [SHELFIE_EXE, "w", f_vals[0], f_vals[1], f_vals[2], f_vals[3] ]
+    return exec_command(cmd)
+
+def calculate_quadrant(f_vals):
+    cmd = [SHELFIE_EXE, "q", f_vals[0], f_vals[1], f_vals[2], f_vals[3] ]
+    return exec_command(cmd)
+
+def exec_command(cmd):
+    proc = subprocess.Popen(cmd, shell=False, stdout=subprocess.PIPE)
+    output = proc.stdout.read()
+    values = output.split()
+    return values
+    
 
 def remove_item(result):
     print "removing item"
@@ -35,3 +77,4 @@ def remove_item(result):
 # start process
 if __name__ == "__main__":
     main()
+
